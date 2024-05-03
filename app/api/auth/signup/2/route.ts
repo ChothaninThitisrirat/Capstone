0f24e52a7a70d35b538977bdfc59fdbcb67add9b
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-03 10:53:30 +0700

    fixsignup

--- app/api/auth/signup/2/route.ts
+++ app/api/auth/signup/2/route.ts
@@ -2,38 +2,6 @@ import { NextResponse } from "next/server"
 import { hash } from 'bcrypt';
 import { prisma } from "@/lib/db";
 
-export async function Get(req: Request) {
-    try {
-        const { username , email} = await req.json()
-        
-        const usernameexist = await prisma.user.findUnique({
-            where: { username:username}
-        })
-        if (usernameexist) {
-            return NextResponse.json({ user:null, message: "Username already exist."},{status: 409})
-        }
-
-        const emailexist = await prisma.user.findUnique({
-            where: { email:email}
-        })
-        if (emailexist) {
-            return NextResponse.json({ user:null, message: "Email already exist."},{status: 409})
-        }
-
-        return NextResponse.json({
-            user:null
-        })
-    } catch (error) {
-        
-        console.log(error);
-        
-        return NextResponse.json({
-            message: "Error",
-            error
-        },{status: 409})
-    }
-}
-
 export async function POST(req: Request) {
     try {
         const { username , email , password , card_id , phone_number , first_name , last_name , address } = await req.json()
@@ -45,15 +13,22 @@ export async function POST(req: Request) {
         const card_idexist = await prisma.user.findUnique({
             where: { card_id:card_id}
         })
-        if (card_idexist) {
-            return NextResponse.json({ user:null, message: "ID Card already exist."},{status: 409})
-        }
-
         const phone_numberexist = await prisma.user.findUnique({
             where: { phone_number:phone_number}
         })
-        if (phone_numberexist) {
-            return NextResponse.json({ user:null, message: "Phone number already exist."},{status: 409})
+
+        if (card_idexist && phone_numberexist) {
+            return NextResponse.json({ 
+                user: null, message: "ID Card and Phone number already exist."},{status: 409}
+            )
+        } else if (card_idexist) {
+            return NextResponse.json({ 
+                user: null, message: "ID Card already exist."},{status: 409}
+            )        
+        } else if (phone_numberexist) {
+            return NextResponse.json({ 
+                user: null, message: "Phone number already exist."},{status: 409}
+            )
         }
         
         const hashpassword = await hash(password , 5)
