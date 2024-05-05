8be89a088b24080343df2662088805e9f890a0e2
Chanchanon Rangpeung <110900547+SuperDoggez@users.noreply.github.com>
2024-05-05 16:24:40 +0700

    category color 01

--- app/page.tsx
+++ app/page.tsx
@@ -29,17 +29,30 @@ export default function Home() {
             className='h-1/6  object-contain ml-5'/>
           <h1 className='text-2xl font-bold text-white mt-8'> เว็บไซต์แลกเปลี่ยนหนังสือที่จะทำให้คุณเพลิดเพลินไปกับหนังสือหลากหลายประเภท</h1>
           <div className="relative w-2/6 mt-8">
-            <input type="search" id="search" className="block rounded-3xl w-full p-4 ps-4 text-sm text-gray-900" placeholder="กรอกชื่อหนังสือที่ต้องการค้นหาได้ที่นี่" required />
+            <input type="search" id="search" className="block rounded-3xl w-full p-4 ps-4 text-l text-gray-900" placeholder="กรอกชื่อหนังสือที่ต้องการค้นหาได้ที่นี่" required />
             <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-dark1 hover:bg-dark2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 ">Search</button>
           </div>
         </div>
 
 
-        <div className='flex flex-wrap items-center w-full  bg-white h-1/6'>
-          <div className=''>
-            <div>นวนิยาย</div>
-            <div>นวนิยาย</div>
-            <div>นวนิยาย</div>
+        <div className='flex justify-center items-center w-full  bg-white h-1/6'>
+         <div className='grid grid-cols-5 gap-x-40 gap-y-4'>
+
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg  from-novel to-white hover:shadow-xl rounded-2xl text-black'>นวนิยาย</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-horror2 to-horror1 hover:shadow-xl rounded-2xl text-white'>สยองขวัญ</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-cartoon to-white hover:shadow-xl rounded-2xl text-black'>การ์ตูน</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-romantic to-white hover:shadow-xl rounded-2xl text-black'>โรแมนติก</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-science to-white hover:shadow-xl rounded-2xl text-black'>วิทยาศาสตร์</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-business to-white hover:shadow-xl rounded-2xl text-black'>การเงิน - ลงทุน</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-education to-white hover:shadow-xl rounded-2xl text-black'>การศึกษา</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-travel to-white hover:shadow-xl rounded-2xl text-black'>ท่องเที่ยว</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-develop to-white hover:shadow-xl rounded-2xl text-black'>การพัฒนาตนเอง</div>
+          <div className='flex justify-center w-full text-2xl py-3 px-9 bg-gradient-to-r shadow-lg from-health to-white hover:shadow-xl rounded-2xl text-black'>สุขภาพ</div>
+
+
+
+
+
           </div>
         </div>
         
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,27 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'dark1': '#363062','dark2': '#435585','dark3': '#818FB4','bg' : '#F7F8F9',
+        'dark1': '#363062',
+        'dark2': '#435585',
+        'dark3': '#818FB4',
+        'bg' : '#F7F8F9',
+        "novel": "#00B2FF",
+        "horror1":  "#FF0000",
+        "horror2":  "#383535",
+        "cartoon":  "#eeff59",
+        "romantic":  "#FF69B4",
+        "science":  "#4efa4b",
+        "business": "#ff7e47",
+        "education": "#47fff3",
+        "develop": "#fffb91",
+        "travel": "#FF4500",
+        "health": "#FF6347",
     },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    },
     },
   },
   plugins: [],
