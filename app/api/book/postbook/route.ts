a92e194b09957c267f28abea4dbb99865b1c4050
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-08 23:31:25 +0700

    fix-postbook

--- app/api/book/postbook/route.ts
+++ app/api/book/postbook/route.ts
@@ -1,33 +1,57 @@
 import { NextResponse } from "next/server";
 import { prismadb } from "@/lib/db";
+import { upLoadIMG } from "@/utils/supabase";
+import { json } from "stream/consumers";
 
 export async function POST(req: Request) {
     try {
-        const { user_id , title , description , picture , pickup , category } = await req.json()
-
-        const date = new Date()
-
-        const newBook = await prismadb.book.create({
-            data: {
-                user_id,
-                title,
                 description,
-                picture,
-                pickup,
-                datetime:date.toISOString(),
-                status:"Post trade",
+            const formData = await req.formData();
+
+            const date = new Date()
+
+            const categoryIds: number[] = []
+            
+            const categoryFormData = formData.getAll('category');
+
+            categoryFormData.forEach((id: FormDataEntryValue) => {
+                if (typeof id === 'string') {
+                    categoryIds.push(parseInt(id));
+                }
+            });
+
+            let image: string[] = [];
+            for await (const [name , value] of formData.entries()){
+                if (name === 'image' && value instanceof File) {
+                    const imageUrl = await upLoadIMG(value);
+                    if (imageUrl) {
+                        image.push(imageUrl);
+                    }
+                }
+            }
+
+        const newbook = await prismadb.book.create({
+            data : {
+                user_id: parseInt(formData.get('user_id') as string),
+                title: formData.get('title') as string,
+                picture: image,
+                description: formData.get('description') as string,
+                status:'Post trade',
+                pickup: formData.get('pickup') as string,
+                datetime: date.toISOString(),
                 req_count:0,
-                category:{
-                    connect:category    
+                category: {
+                    connect: categoryIds.map(id => ({ id }))
                 }
             }
         })
 
         return NextResponse.json({
-            book: newBook,
+            book: newbook,
             message: "Post book successfully"
         },{ status:201 }
     )
+    
 
     } catch (error) {
         console.log(error);
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,23 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { v4 as uuid } from "uuid";
 
-export const createClient = () =>
+const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
+
+export const supabase = createClient()
+
+export const upLoadIMG = async (file: any) => {
+  const fileName = "images/" + uuid() + ".jpg"  ;
+  const { error } = await supabase.storage
+    .from("b-trade")
+    .upload(fileName, file, { cacheControl: "image/jpg"});
+  if (error) {
+    throw error;
+  }
+  const { data } = await supabase.storage.from("b-trade").getPublicUrl(fileName);
+  return data.publicUrl;
+  
+};
