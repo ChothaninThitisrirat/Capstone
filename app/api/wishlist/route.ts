29aa017792a5f847e1688f4198cc82a5b2ea72c4
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-20 17:23:10 +0700

    wishlist

---
+++ app/api/wishlist/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { prismadb } from "@/lib/db";
+
+export async function POST(req: Request) {
+    try {
+        const { user_id , book_id } = await req.json()
+
+        const existWishlist = await prismadb.wishlist.findFirst({
+            where: { 
+                user_id:user_id,
+                book_id:book_id
+            }
+        })
+
+        if (existWishlist) {
+            const deleteWishlist = await prismadb.wishlist.deleteMany({
+                where: { 
+                    user_id:user_id,
+                    book_id:book_id
+                }
+            })
+            
+            return NextResponse.json({
+                user:deleteWishlist,
+                message: "Delete wishlist"
+            }, { status:201 })
+        }
+
+
+
+        const addWishlist = await prismadb.wishlist.create({
+            data: {
+                    user_id,
+                    book_id
+            }
+        })
+
+        return NextResponse.json({
+            wishlist:addWishlist,
+            message: "Book added to wishlist sucessfully."
+        },{ status: 201 }
+    )
+
+    } catch (error) {
+        
+        console.log(error);
+        return NextResponse.json({error})
+        
+    }
+}
