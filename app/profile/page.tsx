363074a839a4ae1f157af5e2f71d6badedfe135a
Chanchanon Rangpeung <110900547+SuperDoggez@users.noreply.github.com>
2024-05-22 10:41:48 +0700

    profile other

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,48 +1,58 @@
 'use client'
 
-import { useSession, signOut } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+
+import React from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import TitleBar from '@/Components/TitleBar'
+import Navbar from '@/Components/Navbar'
+import { HashLoader } from 'react-spinners'
+
 
 export default function Profile() {
-  const { data: session, status } = useSession()
+  const { id } = useParams();
+  const [userData, setUserData] = useState<any>(null)
+  
+  useEffect(() => {
+    if (id){
+      const userID = Array.isArray(id) ? id[0] : id;
+      console.log(id)
+      fetchUserData(userID)
+    }
+  },[id])
 
-  const router = useRouter()
-
-  function IsAdmin() {
-    if (session?.user.isAdmin === true) {
-      return "Admin"
-    } else {
-      return "User"
+  const fetchUserData = async (userID: string) => {
+    try{
+      const response = await fetch(`/api/user/${userID}`)
+      const data = await response.json()
+      setUserData(data)
+      console.log
+    }catch(err){
+      console.log(err)
     }
   }
 
-  
-  useEffect(() => {
-    if (status === 'unauthenticated') {
-      router.push('/login')
-    }
-  }, [status, router])
+  if (!userData) return <div>
+    <div className='w-screen h-screen flex items-center justify-center opacity-95 bg-gradient-to-tr from-yellow-100 to-blue-100'>
+        <HashLoader
+        color='#435585' size={50} aria-label="Loading Spinner" data-testid="loader"/>
+      </div>
+  </div>;
+
 
   return (
-    status === 'authenticated' &&
-    session.user && (
-      <div className="flex h-screen items-center justify-center">
-        <div className="bg-white p-6 rounded-md shadow-md">
-          <p>
-            Welcome, <b>{session.user.username}!</b>
-          </p>
-          <p>Email: {session.user.email}</p>
-          <p>ID: {session.user.id}</p>
-          <p>Role: <IsAdmin /> </p>
-          <button
-            onClick={() => signOut({ callbackUrl: '/' })}
-            className="w-full bg-blue-500 text-white py-2 rounded"
-          >
-            Logout
-          </button>
+    <>
+      <Navbar backGroundOn/>
+      <div className="flex">
+        <div className="container mx-auto p-4">
+          <h1 className="text-2xl font-bold mb-4">User Profile</h1>
+          <div className="bg-white shadow-md rounded-lg p-6">
+            <p className="mb-2"><strong>Name:</strong> {userData.name}</p>
+            <p className="mb-2"><strong>Email:</strong> {userData.email}</p>
+
+          </div>
         </div>
       </div>
-    )
+    </>
   )
 }
