cf7edec91e5ab2a454029147609c10bb58767eb6
Monthol <97272854+ChampMo@users.noreply.github.com>
2024-05-26 17:51:35 +0700

    .

--- app/bookinfo/TradeProcess4.tsx
+++ app/bookinfo/TradeProcess4.tsx
@@ -81,18 +81,18 @@ const TradeProcess3: React.FC<TradeProcess3Props> = ({ bookId, setStateProcess, 
 
             <div
             style={{minHeight: "800px"}}
-            className="flex flex-col items-center h-auto w-sceen z-30 scale-50 sm:scale-100 -translate-y-40 sm:translate-y-0">
+            className="flex flex-col items-center h-auto w-sceen z-30 scale-75 sm:scale-100 -translate-y-32 sm:translate-y-0">
                 <Icon icon="carbon:checkmark-outline" width="70" height="70" className='text-green-400 z-30 mt-5'/>
                 <div className="flex z-30  mt-2 text-3xl">ดำเนินการส่งคำขอเสร็จสิ้น</div>
                 <div className="flex z-30  mt-3 text-xl">B-Trade ID #{idTrade}</div>
-                <div className="flex  gap-20 mt-8">
+                <div className="flex gap-5 sm:gap-20 mt-8">
                     <div className="flex flex-col z-30">
                         <div className="flex text-3xl font-bold mx-auto mb-5">{bookInfo.bookinfo.title}</div>
                         <div className="flex w-full">
                             <img
                             src={bookInfo.bookinfo.picture[0]}
                             alt="Picture"
-                            className=' mx-auto w-56 h-80 object-cover bg-dark3 rounded-sm shadow-sm duration-300'
+                            className=' mx-auto w-56 h-80 object-cover bg-dark3 rounded-sm shadow-sm duration-300 shrink-0'
                             />
                         </div>
                         <div className="flex items-center gap-3 mx-auto mt-5">
@@ -101,7 +101,7 @@ const TradeProcess3: React.FC<TradeProcess3Props> = ({ bookId, setStateProcess, 
                                 <img
                                 src={bookInfo.user.profile_picture}
                                 alt="Profile picture"
-                                className=' w-10 h-10 object-cover cursor-pointer bg-dark3 rounded-full shadow-sm duration-300'
+                                className=' w-10 h-10 object-cover cursor-pointer bg-dark3 rounded-full shadow-sm duration-300 shrink-0'
                                 />
                             </div>
                             <div className="flex">{bookInfo.user.username}</div>
@@ -118,7 +118,7 @@ const TradeProcess3: React.FC<TradeProcess3Props> = ({ bookId, setStateProcess, 
                             <img
                             src={bookSelectTrade?.bookinfo.picture[0]}
                             alt="Profile picture"
-                            className=' mx-auto w-56 h-80 object-cover bg-dark3 rounded-sm shadow-sm duration-300'
+                            className=' mx-auto w-56 h-80 object-cover bg-dark3 rounded-sm shadow-sm duration-300 shrink-0'
                             />
                         </div>
                         <div className="flex items-center gap-3 mx-auto mt-5">
@@ -127,7 +127,7 @@ const TradeProcess3: React.FC<TradeProcess3Props> = ({ bookId, setStateProcess, 
                                 <img
                                 src={bookSelectTrade?.user.profile_picture}
                                 alt="Profile picture"
-                                className=' w-10 h-10 object-cover cursor-pointer bg-dark3 rounded-full shadow-sm duration-300'
+                                className=' w-10 h-10 object-cover cursor-pointer bg-dark3 rounded-full shadow-sm duration-300 shrink-0'
                                 />
                             </div>
                             <div className="flex">{bookSelectTrade?.user.username}</div>
