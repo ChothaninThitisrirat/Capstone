930b6e4eb83d8837154fc35e496f879254c79545
Chanchanon Rangpeung <chanchanon.rang@kmutt.ac.th>
2024-06-02 23:44:10 +0700

    fix review

--- app/api/review/user/route.ts
+++ app/api/review/user/route.ts
@@ -5,6 +5,33 @@ export async function POST(req: Request) {
     try {
         const { user_id, reviewer_id, score, title, describe, trade_id } = await req.json()
 
+        if (trade_id) {
+            const reviewbook = await prismadb.review_User.create({
+                data: {
+                    user_id, 
+                    reviewer_id: parseInt(reviewer_id), 
+                    score, 
+                    title, 
+                    describe
+                }
+            })
+
+            await prismadb.trade.update({
+                where: {
+                    id: parseInt(trade_id)
+                },
+                data: {
+                    isReview:true
+                }
+            })
+
+            return NextResponse.json({
+                review:reviewbook,
+                message: "Review user sucessfully."
+            },{ status:201 })
+
+        }
+
         const reviewbook = await prismadb.review_User.create({
             data: {
                 user_id, 
@@ -14,17 +41,6 @@ export async function POST(req: Request) {
                 describe
             }
         })
-
-        if (trade_id) {
-            await prismadb.trade.update({
-                where: {
-                    id: parseInt(trade_id)
-                },
-                data: {
-                    isReview:true
-                }
-            })
-        }
 
         return NextResponse.json({
             review:reviewbook,
